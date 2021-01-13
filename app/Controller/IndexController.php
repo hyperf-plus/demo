25175e6090ef05bb69845af5015496e020aa418a
Smile <4213509@qq.com>
2021-01-13 19:52:32 +0800

    更新hyperf2.1

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -17,8 +17,6 @@ class IndexController extends AbstractController
     {
         $user = $this->request->input('user', 'Hyperf');
         $method = $this->request->getMethod();
-
         return $this->response->redirect('/auth');
-
     }
 }
